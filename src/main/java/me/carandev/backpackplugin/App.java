76430207f9c60f986cd1d10a290496c57e34e4c3
Carlos Gomez <carlosandres0741@gmail.com>
2022-12-19 12:16:26 -0500

    Created template

---
+++ src/main/java/me/carandev/backpackplugin/App.java
@@ -0,0 +1,19 @@
+package me.carandev.backpackplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    getLogger().info("Hola, está de locos SpigotMC, es maravilloso!");
+  }
+
+  @Override
+  public void onDisable() {
+    getLogger().info("Adios, SpigotMC!");
+  }
+}
